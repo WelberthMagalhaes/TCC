858cdf565f732ac13d5fd394a60ef80b0314b079
WelberthMagalhaes <welberthheider@gmail.com>
2022-05-30 23:18:24 -0300

    pronto para gerar dados

--- Code/codeTcc.py
+++ Code/codeTcc.py
@@ -8,7 +8,7 @@ import openpyxl
 import string
 
 
-entrada = Problema('C:/Users/wheidermagal/OneDrive - DXC Production/Documents/Pessoal/TCC/inputs/input.xls')
+entrada = Problema('C:/Users/wheidermagal/OneDrive - DXC Production/Documents/Pessoal/TCC/inputs/input1.xlsx')
 def gravaResultado():
 	qtdCompradaX = pd.DataFrame(index=N, columns=range(1,T))
 	qtdEstocadaY = pd.DataFrame(index=N, columns=range(1,T))
@@ -31,6 +31,7 @@ e = entrada.ei
 v = entrada.vi
 A = entrada.A
 
+
 m = Model("MIN custos totais") # Nome do modelo
 
 x = {(i, j): m.add_var(obj=0, var_type=INTEGER, name="x(%s,%d)" % (i, j))
@@ -42,14 +43,13 @@ y = {(i, j): m.add_var(obj=0, var_type=INTEGER, name="y(%s,%d)" % (i, j))
 		for j in range(T)}
 
 
-m.objective = minimize( xsum((c[i][j-1] * x[i,j]) + (h[i]*y[i,j]) for i in N for j in range(T)))
+m.objective = minimize( xsum((c[i][j-1] * x[i,j]) + (h[i][j-1]*y[i,j]) for i in N for j in range(T)))
 
 # Restrição 1: Insere o estque inicial
 for i in N:
 	m += y[i,0] == s[i]
 
 # Restrição 2: equaliza o estoque. Estoque atual = estoque inicial + Qtd comprada - demanda
-# no modelo está excluindo o 0 do conjunto N. Não teria que excluí-lo do T?
 for i in N:
 	for t in range(1,T):
 		m += y[i,t] == y[i,t-1] + x[i,t] - d[i][t-1]
@@ -68,11 +68,14 @@ for i in N:
 		m += x[i,t] >=0
 
 
-m.optimize()
-
 m.write("teste.lp")
+m.optimize()
 m.write("solucao.sol")
+
+
 gravaResultado()
+
+# quando alterar o T eu uso uma função para gerar todas as instâncias de entrada de acordo com o numero T
 
 #acessar variável
 #print(x['a1',0])
--- Code/problema.py
+++ Code/problema.py
@@ -1,14 +1,20 @@
-from pprint import pp
+from ast import While
+from ctypes import sizeof
+from turtle import home
 import pandas as pd
 import numpy as np
+import random
+import openpyxl
 
 
 class Problema:
-		
+
+	"""
 	def __init__(self, arq):
 		
 		#leitura do arquivo
 		df = pd.read_excel(arq, sheet_name=0)
+		book = openpyxl.load_workbook(arq, data_only=True)
 
 		# N - Conjunto de Toners
 		aux = df.columns.to_list()
@@ -19,9 +25,6 @@ class Problema:
 		# T - Número de períodos
 		self.T = df.iat[3,1] 
 		#--------------------
-
-		#indice de coluna para custo e demanda de toner
-		#col = int(self.T+1)
 
 		# A - Capacidade de Armazenamento
 		self.A = df.iat[5,1]
@@ -34,21 +37,13 @@ class Problema:
 		s = s.drop(columns=0)
 		ind = s.index.to_list()
 		self.si = {ind[i] : s.values[i][0] for i in range(s[1].size)}
+		#print("Estoque Inicial:\n"+str(self.si))
 		#--------------------
-
+		
+		col = int(self.T+1)
 		# ci - Custo unitário do toner i
 		df = pd.read_excel(arq, sheet_name=5)
-		col = int(self.T+1)
 		self.ci = {df.values[i][0] : df.values[i][1:col] for i in range(df['toner'].size)}
-
-		"""c = df
-		c = c.dropna()
-		c = c.drop(1)
-		c = c.T
-		c = c.drop('Conjunto de Toners')
-		ind = c.index.to_list()
-		self.ci = {ind[i] : c.values[i][0] for i in range(c[0].size)}"""
-
 		#--------------------
 
 		# vi - Volume ocupado por cada toner #Validado 16/12
@@ -60,14 +55,126 @@ class Problema:
 		df = pd.read_excel(arq, sheet_name=2)
 		self.ei = {df.values[i][0] : df.values[i][1] for i in range(df['toner'].size)}
 		#--------------------
+
 		
 		# dit - demanda média esperada do toner i no período t  #Validado 16/12
 		df = pd.read_excel(arq, sheet_name=3)
-		col = int(self.T+1)
 		self.di = {df.values[i][0] : df.values[i][1:col] for i in range(df['toner'].size)}
 		#--------------------
 
 		# hi - Custo de capital do estoque por unidade do toner i
 		df = pd.read_excel(arq, sheet_name=4)
-		self.hi = {df.values[i][0] : df.values[i][1] for i in range(df['toner'].size)}
+		self.hi = {df.values[i][0] : df.values[i][1:col] for i in range(df['toner'].size)}
 		#--------------------
+	"""
+
+	def __init__(self, arq):
+
+		def geraAleatorios(caminho):
+			book = openpyxl.load_workbook(caminho,data_only=True)
+			sheet1 = book['home']
+			T = int(sheet1['B5'].value)
+
+			
+			#N - número de toners
+
+			N=[]
+			i = 2
+			aux = sheet1.cell(row=1,column=i).value
+			
+			while(aux != None):
+				N.append(aux)
+				i+=1
+				aux = sheet1.cell(row=1,column=i).value
+
+
+			#estoque inicial
+			for i in range(2, len(N)+1):
+				sheet1.cell(row=3,column=i).value = random.randint(1,20)
+				
+				aux = sheet1.cell(row=3,column=i).value * (random.randint(1,5))
+				sheet1.cell(row=3,column=i).value = aux
+			#------
+			sheet4 = book['demandaToner']
+			sheet6 = book['custoToner']
+
+			indice = 1
+			for i in range(2,T+2):
+				#demanda toner
+				sheet4.cell(row=1,column=i).value = "t%d"%(indice)
+				#custo toner
+				sheet6.cell(row=1,column=i).value = "t%d"%(indice)
+				
+				if(i>T):break
+
+				for j in range(2,len(N)+2):
+					#demanda toner
+					aux = sheet4.cell(row=j,column=2).value * random.randint(1,T)
+					sheet4.cell(row=j,column=i+1).value = aux
+					
+					#custo toner
+					aux = sheet6.cell(row=j,column=2).value + (sheet6.cell(row=j,column=2).value * random.random())
+					sheet6.cell(row=j,column=i+1).value = aux
+				indice += 1
+
+			book.save(caminho)
+			book.close
+		
+		#gerando dados aleatórios
+		geraAleatorios(arq)
+		
+		#leitura do arquivo
+		df = pd.read_excel(arq, sheet_name=0)
+		book = openpyxl.load_workbook(arq, data_only=True)
+
+		# N - Conjunto de Toners
+		aux = df.columns.to_list()
+		aux.pop(0)
+		self.N = aux
+		#--------------------
+
+		# T - Número de períodos
+		self.T = df.iat[3,1] 
+		#--------------------
+
+		# A - Capacidade de Armazenamento
+		self.A = df.iat[5,1]
+		#--------------------
+
+		#si - Estoque inicial do toner i
+		s = df
+		s = s.T
+		s = s.drop('Conjunto de Toners')
+		s = s.drop(columns=0)
+		ind = s.index.to_list()
+		self.si = {ind[i] : s.values[i][0] for i in range(s[1].size)}
+		#print("Estoque Inicial:\n"+str(self.si))
+		#--------------------
+		
+		col = int(self.T+1)
+		# ci - Custo unitário do toner i
+		df = pd.read_excel(arq, sheet_name=5)
+		self.ci = {df.values[i][0] : df.values[i][1:col] for i in range(df['toner'].size)}
+		#--------------------
+
+		# vi - Volume ocupado por cada toner #Validado 16/12
+		df = pd.read_excel(arq, sheet_name=1)
+		self.vi = {df.values[i][0] : df.values[i][1] for i in range(df['toner'].size)}
+		#--------------------
+
+		# ei - margem mínima média do toner i no período t (estoque de segurança) #Validado 16/12
+		df = pd.read_excel(book, sheet_name=2,engine="openpyxl")
+		self.ei = {df.values[i][0] : df.values[i][1] for i in range(df['toner'].size)}
+		#--------------------
+
+		
+		# dit - demanda média esperada do toner i no período t  #Validado 16/12
+		df = pd.read_excel(arq, sheet_name=3)
+		self.di = {df.values[i][0] : df.values[i][1:col] for i in range(df['toner'].size)}
+		#--------------------
+
+		# hi - Custo de capital do estoque por unidade do toner i
+		df = pd.read_excel(book, sheet_name=4,engine="openpyxl")
+		self.hi = {df.values[i][0] : df.values[i][1:col] for i in range(df['toner'].size)}
+		#--------------------
+		#TODO Passar as contas para o código e gerar os dados da planilha partir do código.
